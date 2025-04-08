63ba09ba7a1ae6f7f7cf784447118a4fa1355763
Rishnu Dk <rishnukrishnan@gmail.com>
2025-04-08 09:56:45 +0530

    fixed button css issues

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,18 +1,4 @@
-        // function togglePassword(fieldId) {
-        //     const field = document.getElementById(fieldId);
-        //     const icon = field.nextElementSibling.querySelector('i');
-            
-        //     if (field.type === 'password') {
-        //         field.type = 'text';
-        //         icon.classList.remove('fa-eye');
-        //         icon.classList.add('fa-eye-slash');
-        //     } else {
-        //         field.type = 'password';
-        //         icon.classList.remove('fa-eye-slash');
-        //         icon.classList.add('fa-eye');
-        //     }
-        // }
-       
+
 
         
         function updatePreview() {
